0fba607c63699fe32a211e2c2af9145d9b066377
Virginia Thayer <virginia@stripe.com>
2019-07-24 16:17:53 -0700

    Rendering some team member details in DOM from team array.

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState }  from 'react';
+import React from 'react';
 import './App.css';
 import { Form } from './components/Form';
 
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="App">
-      {/* <MemberList /> */}
+      
       <Form />
     </div>
   );
--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -2,59 +2,81 @@ import React, { useState } from 'react';
 
 export function Form() {
     const [team, setTeam] = useState([]);
-    const [member, setMember] = useState({name: '', email: '', role: ''});
+    const [member, setMember] = useState({ name: '', email: '', role: '' });
 
     function handleChange(event) {
         const updatedMember = { ...member, [event.target.name]: event.target.value };
         console.log(
-          "handleChange",
-          event.target.name,
-          event.target.value,
-          updatedMember
+            "handleChange",
+            event.target.name,
+            event.target.value,
+            updatedMember
         );
         setMember(updatedMember);
-      }
-    
-      function handleSubmit(event) {
+    }
+
+    function handleSubmit(event) {
         event.preventDefault();
+        team.push(member);
+        setTeam(team);
         console.log("team: ", team);
-        setTeam(team.push(member));
+    }
 
-      }
-
-    console.log('Form');
     return (
+        <div>
+            <form onSubmit={event => handleSubmit(event)}>
+                <label>
+                    Name:
+          <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        placeholder="name of you"
+                        onChange={handleChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    Email:
+          <input
+                        type="text"
+                        id="email"
+                        name="email"
+                        placeholder="email of you"
+                        onChange={handleChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    Role:
+          <input
+                        type="text"
+                        id="role"
+                        name="role"
+                        placeholder="role of you"
+                        onChange={handleChange}
+                    />
+                </label>
 
-        <form onSubmit={event => handleSubmit(event)}>
-        <label>
-          Name:
-          <input
-            type="text"
-            id="name"
-            name="name"
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Email:
-          <input
-            type="text"
-            id="email"
-            name="email"
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Role:
-          <input
-            type="text"
-            id="role"
-            name="role"
-            onChange={handleChange}
-          />
-        </label>
-        <button>Submit!</button>
-      </form>
+                <button>Submit!</button>
+            </form>
+
+            <div className="memberList">
+                <h2> List of Members</h2>
+                <div>
+                {team.map((member) => {
+                    console.log('render element for ', member);
+                    return (
+                        <div key={member.name}>
+                        <p>Name: {member.name} 
+                            Email: {member.email} 
+                            Role: {member.role}</p>
+                        </div>
+                    )
+                })}
+                </div>
+            </div>
+        </div>
 
     );
 }
